af76f5951adcc6a6dc2e6ae6348df59327ce2865
Raphael Peters <rappet@rappet.de>
2020-06-06 23:57:17 +0200

    Changed configuration file format

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -7,6 +7,18 @@ use crate::conf::Role;
 pub fn check_command(matches: &clap::ArgMatches) -> Result<(), Box<dyn Error>> {
     let path = Path::new(matches.value_of("config").unwrap());
     let role = Role::open(path)?;
+
+    for (module, tasks) in &role.tasks {
+        for (name, task) in tasks {
+            info!(
+                "Found task: {}.{} {}",
+                module, name,
+                task.description.clone().unwrap_or("<with no description>".into())
+            );
+        }
+    }
+
+    info!("Role content: {}", serde_json::to_string_pretty(&role)?);
     println!("Parsed {} tasks.", role.tasks.len());
     println!("{} looks like a valid role.", path.display());
 
--- src/conf/role.rs
+++ src/conf/role.rs
@@ -1,5 +1,3 @@
-use crate::module::Module;
-
 use serde::{Serialize, Deserialize};
 
 use std::collections::BTreeMap;
@@ -11,9 +9,13 @@ use std::fs;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Role {
     #[serde(default)]
-    pub vars: BTreeMap<String, serde_json::Value>,
-    #[serde(rename = "task")]
-    pub tasks: Vec<Task>,
+    pub version: u32,
+    #[serde(default)]
+    pub vars: BTreeMap<String, Var>,
+    #[serde(default)]
+    pub locals: BTreeMap<String, serde_json::Value>,
+    #[serde(default)]
+    pub tasks: BTreeMap<String, BTreeMap<String, Task>>,
 }
 
 impl Role {
@@ -27,13 +29,37 @@ impl Role {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum StringOrVec {
+    String(String),
+    Vec(Vec<String>)
+}
+
+impl Default for StringOrVec {
+    fn default() -> StringOrVec {
+        StringOrVec::Vec(Vec::new())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Var {
+    #[serde(default)]
+    required: bool,
+    #[serde(default)]
+    description: Option<String>,
+    #[serde(rename = "type")]
+    var_type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Task {
-    pub name: String,
-    pub module: Module,
+    pub description: Option<String>,
 
     #[serde(default = "empty_toml_table", flatten)]
     pub params: toml::value::Table,
 
-    pub requires: Option<String>,
-    pub yields: Option<String>,
+    #[serde(default)]
+    pub require: StringOrVec,
+    #[serde(default)]
+    pub yields: StringOrVec,
 }
